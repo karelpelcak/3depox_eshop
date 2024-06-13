c21f30fb32cd8db64bd6c544bc66e825c8c60f47
Pupa's dev <kaja.pelcak@seznam.cz>
2024-06-13 15:07:48 +0200

    trying some api for server setup

--- src/routes/(auth)/register/+page.svelte
+++ src/routes/(auth)/register/+page.svelte
@@ -1,1 +1,17 @@
-<h1>register</h1>
+<script>
+    let message = "h"
+
+    async function fetchMessage() {
+        try {
+            const response = await fetch('/register/api');
+            const data = await response.json();
+            console.log(data);
+            message = data;
+        } catch (error) {
+            console.error('Error fetching message:', error);
+        }
+    }
+</script>
+
+<button on:click={fetchMessage}>Get Message</button>
+<p>{message}</p>
