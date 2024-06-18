f69fa17f3da6cfe5bc80236b8c3d6a7ca2602126
karelpelcak <gryvdycz@gmail.com>
2024-06-18 21:29:14 +0200

    fs

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,8 @@
 import adapter from "@sveltejs/adapter-node";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import dotenv from "dotenv";
+/** @type {import('@sveltejs/kit').Config} */
+dotenv.config();
 const config = {
   kit: {
     adapter: adapter(),
